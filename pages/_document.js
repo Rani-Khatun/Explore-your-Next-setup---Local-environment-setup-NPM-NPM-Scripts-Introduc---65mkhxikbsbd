fd33936e71a5dd2815eda27264656ee27c50c2c2
Rani-Khatun <ranikhatun1078@gmail.com>
2024-07-02 21:18:16 +0530

    added

--- pages/_document.js
+++ pages/_document.js
@@ -11,3 +11,18 @@ export default function Document() {
     </Html>
   )
 }
+
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
